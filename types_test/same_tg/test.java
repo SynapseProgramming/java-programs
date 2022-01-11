6d0744b1a692dad9881b017b5c2952342ab42e2a
ONG TSENG CHEN ROALD <roald@pe113.comp.nus.edu.sg>
2022-01-11 20:34:42 +0800

    added in type tests

---
+++ types_test/same_tg/test.java
@@ -0,0 +1,23 @@
+class wow{
+
+public static void main(String args[]){
+
+// this shows that you can convert from a int to a long 
+int x=3;
+long y;
+y=x;
+System.out.println(y);
+
+// This is not allowed!
+//long z=4;
+//int w=z;
+//System.out.println(w);
+
+
+
+}
+
+
+
+
+};
