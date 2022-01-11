c8e7dedfc47c90718e5292091c5da68f4bc8ba3c
ONG TSENG CHEN ROALD <roald@pe113.comp.nus.edu.sg>
2022-01-11 18:19:48 +0800

    added in circles test code

---
+++ circles_test/circles.java
@@ -0,0 +1,40 @@
+
+
+
+
+
+
+
+class Circle {
+
+double showx(){
+
+return x;
+
+}
+
+
+
+double x;
+double y;
+double z;
+
+
+
+
+}
+
+
+class circ{
+
+public static void main(String args[]){
+Circle obj=new Circle();
+obj.x=69;
+
+System.out.println(obj.showx());
+
+}
+
+
+
+}
