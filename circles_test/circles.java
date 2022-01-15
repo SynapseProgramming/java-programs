febc82ec8316b71fa6c142378e460adaff7d7aa5
Ong Tseng Chen Roald <roald.ong@gmail.com>
2022-01-15 14:58:05 +0800

    added in object reference test

--- circles_test/circles.java
+++ circles_test/circles.java
@@ -1,10 +1,3 @@
-
-
-
-
-
-
-
 class Circle {
 
 double showx(){
@@ -12,7 +5,6 @@ double showx(){
 return x;
 
 }
-
 
 
 double x;
@@ -28,8 +20,15 @@ double z;
 class circ{
 
 public static void main(String args[]){
+// in each java program, we would have one main entry class with the public static void main function
+// in this case, obj2 is a reference to the object 1 instance. Obj2 would only be separate from obj 1 if a new object is assigned to obj2   
 Circle obj=new Circle();
+Circle obj2=obj;
+
+
 obj.x=69;
+// This shows that obj2 is referenced to obj 1
+obj2.x=71;
 
 System.out.println(obj.showx());
 
