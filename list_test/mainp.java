04d42c57575fcb9ea3dd9bc4ddb8dcc93a5b7dfc
Ong Tseng Chen Roald <roald.ong@gmail.com>
2022-01-16 13:23:48 +0800

    applied code formatting to some files.

--- list_test/mainp.java
+++ list_test/mainp.java
@@ -1,28 +1,11 @@
-class Start{
-  
-  public static void main(String args[]){
-  
-  Integer a[] = new Integer[] {1,2,3,4,5};
-  for(int i=0;i<a.length;i++){
-    System.out.println(a[i]);
-  }
+class Start {
+
+  public static void main(String[] args) {
+
+    Integer a[] = new Integer[] {1, 2, 3, 4, 5};
+    for (int i = 0; i < a.length; i++) {
+      System.out.println(a[i]);
+    }
     System.out.println("blank");
-    
-
-
-
-
   }
-
-
-
-
-
-
-
-
-
-  }
-  
-
-
+}
