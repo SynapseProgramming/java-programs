fdc307949fd4ae92ec8f79d61deff554e3de7eaf
Ong Tseng Chen Roald <roald.ong@gmail.com>
2022-01-19 15:42:42 +0800

    added in point class for inheritance test

---
+++ inheritance_test/Point.java
@@ -0,0 +1,18 @@
+class Point {
+
+  private double px;
+  private double py;
+
+  Point(double x, double y) {
+    this.px = x;
+    this.py = y;
+  }
+
+  public double getx() {
+    return this.px;
+  }
+
+  public double gety() {
+    return this.py;
+  }
+}
